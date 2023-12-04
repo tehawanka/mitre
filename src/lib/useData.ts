d1fdc18b5a1401c3d259d07e44f5f1eda15915d7
bmendecki <bar.mendecki@gmail.com>
2023-12-04 20:39:24 +0100

    add eslint & tsconfig

--- src/lib/useData.ts
+++ src/lib/useData.ts
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 export const useData = () => {
-  const [data, setData] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState('')
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
-    async function getData() {
+    async function getData () {
       const resp = await fetch(
-        "https://bartlomiejmendecki.gitlab.io/mitre/mitre.json"
-      );
+        'https://bartlomiejmendecki.gitlab.io/mitre/mitre.json'
+      )
       if (!resp.ok) {
-        throw new Error(`Error occured: ${resp.status}`);
+        throw new Error(`Error occured: ${resp.status}`)
       }
-      const result = await resp.json();
-      setData(result);
-      setLoading(false);
+      const result = await resp.json()
+      setData(result)
+      setLoading(false)
     }
-    getData();
-  }, []);
-  return { data, loading };
-};
+    getData()
+  }, [])
+  return { data, loading }
+}
